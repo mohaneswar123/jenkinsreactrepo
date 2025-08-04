9dd3269fc5dcdffca6ef0f4e5a27488a81c50bb7
Eswar <pvmohaneswar@gmail.com>
2025-08-04 10:01:21 +0530

    upadated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : 'jenkinserp',// deplyment name for jenkins folder not git repoo
 })
